39349bdfc0fb0960fc56459f3c69451a3d4529d0
pravalikareddy-1414 <91274707+pravalikareddy-1414@users.noreply.github.com>
2024-07-23 17:20:53 -0400

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,101 @@
+html, body {
+  height: 100%; /* Ensure the HTML and body take up full height of the viewport */
+  margin: 0; /* Remove default margin */
+  display: flex;
+  justify-content: center; /* Center content horizontally */
+  align-items: center; /* Center content vertically */
+  background-color: #f0f0f0; /* Background color for visibility */
+}
+
+.container {
+width: 450px;
+height: 450px;
+top:64px;
+left:62px
+gap: ;0px;
+opacity: 0px;
+margin: 0 auto;
+padding :50px;
+border :1px solid black;
+radius :2px;
+background: #1F2937;
+border-radius:12px;
+border:#fffff; /* Center items horizontally and vertically */
+}
+
+h1{
+  font-family: Karla;
+  font-size: 40px;
+  font-weight: 800;
+  line-height: 40px;
+  letter-spacing: -0.025em;
+  text-align: left;
+
+}
+.line1{
+    color:white;
+}
+.line2{
+    color:green;
+}
+
+h2{
+    //styleName: text-xl/leading-7/font-normal;
+  font-family: Inter;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+  text-align: left;
+
+}
+
+.button{
+  Width  :191px;
+  Height : 42px;
+   Top :284px;
+   Left :52px;
+   border-radius :6px;
+   Padding :9px,17px,9px,15px;
+   Gap :12px;
+   background: #10B981;
+   font-family: Inter;
+   font-size: 16px;
+   font-weight: 500;
+   line-height: 24px;
+   text-align: left;
+   color: #FFFFFF;
+   text-align:center;
+
+}
+.subheading{
+    color:white;
+}
+.line{
+    width:446px;
+    border: 1px solid white;
+    margin:20px auto;
+    
+
+}
+
+
+
+.button1,.button2{
+    display: inline-block;
+      width: 211px;
+      height: 39px;
+     /* Adjust margin as needed */
+      background: #273549;
+      border-radius: 6px;
+      color: #fff;
+      font-family: Inter;
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 39px; /* Center text vertically */
+      text-align: center;
+
+
+
+}
+
+
---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+function generateRandomPassword(arr, length) {
+  let randomPassword = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    randomPassword += arr[randomIndex];
+  }
+  return randomPassword;
+}
+
+function twoRandomPasswords() {
+  let password1 = generateRandomPassword(characters, 15);
+  let password2 = generateRandomPassword(characters, 15);
+  button1.textContent = password1;
+  button2.textContent = password2;
+  return;
+}
